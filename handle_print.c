b36020c422f6bcd480154695ebf6c00d95721584
Christopher Chilengwe <christopherchilengwe@gmail.com>
2023-08-21 21:23:09 -0700

    update

--- handle_print.c
+++ handle_print.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * handle_print - Prints an argument based on its type
+ * handle_print - Prints an argument based on its type.
  * @fmt: Formatted string in which to print the arguments.
  * @ind: Index of the format specifier in the format string.
  * @list: List of arguments to be printed.
